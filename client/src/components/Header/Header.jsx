89bf627ec70e4c8dd6e64174dc5ed794d265ff45
Yaser Hasan <sweeter.yaser@gmail.com>
2021-09-17 08:48:01 +0300

    1.[Front-end] fixed a bug in header where mobile menu won't close when clicking on the links, 2.[Front-end] some minor improvements in the header.

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
@@ -12,11 +12,25 @@ import Loading from '../Loading';
 function Header() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
     const { user, logoutLoading } = useSelector(state => state.auth);
+    const mobileMenuRef = useRef();
     const dispatch = useDispatch();
 
-    function toggleMobileMenu() {
+    function toggleMobileMenu(e) {
+        e.preventDefault();
         setMobileMenuOpen(prev => !prev);
     }
+
+    // to close mobile menu on link click
+    // this will run every time the auth changes
+    // to add the event to the new links that gets added
+    useEffect(() => {
+        const links = mobileMenuRef.current.querySelectorAll('a');
+        links.forEach(link =>
+            link.addEventListener('click', e => {
+                setMobileMenuOpen(false);
+            })    
+        );
+    }, [user]);
 
     function buildInfoLinks(forMobile) {
         const links = [
@@ -25,24 +39,25 @@ function Header() {
         ];
         if (forMobile) 
             return links.map(link => (
-                <li><Link to={link.path}>{link.name}</Link></li>
+                <li key={link.name}><Link to={link.path}>{link.name}</Link></li>
             ));
         
         return links.map(link => (
-            <li><Link to={link.path}><h5>{link.name}</h5></Link></li>
+            <li key={link.name}><Link to={link.path}><h5>{link.name}</h5></Link></li>
         ));
     }
 
     function buildAccountLinks() {
         if (user)
             return [
-                <li><Link to='/'>My Orders</Link></li>,
-                <li><Link to='/' onClick={() => dispatch(logout())}>Logout</Link></li>,
+                <li key='My Cart'><Link to='/'>My Cart</Link></li>,
+                <li key='My Orders'><Link to='/'>My Orders</Link></li>,
+                <li key='Logout'><Link to='/' onClick={() => dispatch(logout())}>Logout</Link></li>,
             ];
 
         return [
-            <li><Link to='/login'>Login</Link></li>,
-            <li><Link to='/register'>Register</Link></li>,
+            <li key='Login'><Link to='/login'>Login</Link></li>,
+            <li key='Register'><Link to='/register'>Register</Link></li>,
         ]
     }
 
@@ -72,15 +87,16 @@ function Header() {
                     </Styles.DropDownLink>}
                 </Styles.Nav>
 
+                {logoutLoading && <Loading small />}
                 {/* Added '$' prefix on the prop name to avoid React Unknown Prop Warning */}
-                <Styles.NavLink $menuBtn className="clickable" to="/" onClick={toggleMobileMenu}>
+                {!logoutLoading && <Styles.NavLink $menuBtn className="clickable" to="" onClick={toggleMobileMenu}>
                     <i className={mobileMenuOpen ? "fas fa-times" : "fas fa-bars"}></i>
-                </Styles.NavLink>
+                </Styles.NavLink>}
             </Styles.MainHead>
 
             <Divider />
 
-            <Styles.MobileNav active={mobileMenuOpen}>
+            <Styles.MobileNav active={mobileMenuOpen} ref={mobileMenuRef}>
                 <Styles.MobileSearchWrapper>
                     <SearchInput mobile />
                 </Styles.MobileSearchWrapper>
